fb9fe13eca51abb10c6749d70442a73f56c4a78e
yak-fumblepack <mike.liang2006@gmail.com>
2020-12-11 23:04:24 -0500

    checked everything for categories

--- portfolio/models.py
+++ portfolio/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    #category = models.CharField(max_length=255, default='Uncategorized', choices= Category.objects.all().values_list('name','name'))
+    category = models.CharField(max_length=255, default='Uncategorized', choices= Category.objects.all().values_list('name','name'))
     snippet = models.CharField(max_length=255, default='Click to read more...' )
     body = RichTextUploadingField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
