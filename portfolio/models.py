c8edfb38930b6df8ec6b41b2aa57167e6fd6c45e
yak-fumblepack <mike.liang2006@gmail.com>
2020-12-11 22:59:32 -0500

    migrations and email changes

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,10 +5,10 @@ from django.urls import reverse
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
-#class Category(models.Model):
-    #name = models.CharField(max_length=255)
-    #def __str__(self):
-        #return self.name
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 # Add Posts model
 class Post(models.Model):
