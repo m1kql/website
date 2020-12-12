c066f08ab33be7d6872a5a43a66d4d7bff74b3b0
yak-fumblepack <mike.liang2006@gmail.com>
2020-12-11 23:19:48 -0500

    made admin login

--- website/urls.py
+++ website/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-]
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('login/', include('django.contrib.auth.urls')),
+    path('login/', include('login.urls')),
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
